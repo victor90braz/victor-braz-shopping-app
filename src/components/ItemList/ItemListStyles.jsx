df84ea2b9fe1abfc85e7cca426132fd3fea14608
victor90braz <victor.90braz@gmail.com>
2023-08-23 20:09:10 +0200

    Add  justify-content to ItemListStyles

--- src/components/ItemList/ItemListStyles.jsx
+++ src/components/ItemList/ItemListStyles.jsx
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const ItemListStyles = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(22rem, 1fr));
+  justify-content: center;
   justify-items: center;
 `;
