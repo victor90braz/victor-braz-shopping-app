7b8cbdec4d424f01f0f53d364221856517f90f55
victor90braz <victor.90braz@gmail.com>
2023-08-23 20:19:35 +0200

    Install home icon from FontAwesomeIcon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 // @ts-nocheck
 import { useLocation } from "preact-iso";
 import { HeaderNavStyle } from "./HeaderStyles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
+import { faHome } from "@fortawesome/free-solid-svg-icons"; // Import the home icon
 
 export function Header() {
   const { url } = useLocation();
   return (
     <HeaderNavStyle>
       <a href="/" class={url === "/" ? "active" : ""}>
-        Home
+        <FontAwesomeIcon icon={faHome} /> Home {/* Add icon */}
       </a>
     </HeaderNavStyle>
   );
