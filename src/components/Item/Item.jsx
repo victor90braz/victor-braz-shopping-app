fff172d09c3d0a70d53b96d218b4d93cd04cbb15
victor90braz <victor.90braz@gmail.com>
2023-08-24 00:37:59 +0200

    Manager customize the output

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,6 +3,9 @@
 import { ItemStyle } from "./ItemStyles";
 
 const Item = ({ product }) => {
+  const priceContent =
+    product.price == 0 ? "Not Available" : `${product.price}€`;
+
   return (
     <ItemStyle>
       <div className="card">
@@ -18,7 +21,7 @@ const Item = ({ product }) => {
           <h5 className="card-title">{product.brand}</h5>
           <div className="card-text">
             <p>Model: {product.model}</p>
-            <p>Price: {product.price}€</p>
+            <p>Price: {priceContent}</p>
           </div>
           <a href="#" className="btn btn-primary">
             Detail
