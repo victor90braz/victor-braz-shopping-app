aa4f6700784ee715c2e3aaf50679261973717af6
victor90braz <victor.90braz@gmail.com>
2023-08-23 09:40:22 +0200

    Update Header component path and redifine folder structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { render } from "preact";
 import { LocationProvider, Router, Route } from "preact-iso";
 
-import { Header } from "./components/Header.jsx";
+import { Header } from "./components/Header/Header.jsx";
 import { NotFound } from "./pages/_404.jsx";
 import "./style.css";
 import MainView from "./pages/MainView/MainView.jsx";
