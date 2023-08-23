90ad27ebcd62854b16303135505e7b1b3e908fdd
victor90braz <victor.90braz@gmail.com>
2023-08-23 18:45:20 +0200

    Resize item style

--- src/components/Item/ItemStyles.jsx
+++ src/components/Item/ItemStyles.jsx
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const ItemStyle = styled.div`
   .card {
-    width: 22rem;
+    width: 28rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .card-img-top {
+    max-width: 50%; /* Set maximum width for the image */
   }
 
   .card-body {
