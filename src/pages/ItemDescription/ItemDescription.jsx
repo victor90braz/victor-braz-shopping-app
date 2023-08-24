ef008911c0c41da0f7870bfe01ebeefbc32acc67
victor90braz <victor.90braz@gmail.com>
2023-08-24 20:43:47 +0200

    Add spinner on loading

--- src/pages/ItemDescription/ItemDescription.jsx
+++ src/pages/ItemDescription/ItemDescription.jsx
@@ -4,6 +4,7 @@ import { ItemDescriptionStyles } from "./ItemDescriptionStyles";
 
 const ItemDescription = () => {
   const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const id = "ZmGrkLRPXOTpxsU4jjAcv";
@@ -13,44 +14,51 @@ const ItemDescription = () => {
       .then((response) => response.json())
       .then((data) => {
         setProduct(data);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <ItemDescriptionStyles>
-      <div class="card" style="width: 52rem">
-        <img
-          src={product.imgUrl}
-          alt={product.name}
-          className="full-width-image"
-        />
-        <div class="card-body">
-          <h5 class="card-title">{product.brand}</h5>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Marca: {product.brand}</li>
-            <li class="list-group-item">Modelo: {product.model}</li>
-            <li class="list-group-item">Precio: {product.price}€</li>
-            <li class="list-group-item">CPU: {product.cpu}</li>
-            <li class="list-group-item">RAM: {product.ram}</li>
-            <li class="list-group-item">Sistema Operativo: {product.os}</li>
-            <li class="list-group-item">
-              Resolución de pantalla: {product.screenResolution}
-            </li>
-            <li class="list-group-item">Batería: {product.battery}</li>
-            <li class="list-group-item">Cámaras: {product.cameras}</li>
-            <li class="list-group-item">Dimensiones: {product.dimensions}</li>
-            <li class="list-group-item">Peso: {product.weight}</li>
-          </ul>
-          <div class="card-actions">
-            <a href="#" class="card-link">
-              Action
-            </a>
-            <a href="#" class="card-link">
-              Action
-            </a>
+      {isLoading ? (
+        <div class="spinner-border text-primary" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <div class="card" style="width: 52rem">
+          <img
+            src={product.imgUrl}
+            alt={product.name}
+            className="full-width-image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{product.brand}</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Marca: {product.brand}</li>
+              <li class="list-group-item">Modelo: {product.model}</li>
+              <li class="list-group-item">Precio: {product.price}€</li>
+              <li class="list-group-item">CPU: {product.cpu}</li>
+              <li class="list-group-item">RAM: {product.ram}</li>
+              <li class="list-group-item">Sistema Operativo: {product.os}</li>
+              <li class="list-group-item">
+                Resolución de pantalla: {product.screenResolution}
+              </li>
+              <li class="list-group-item">Batería: {product.battery}</li>
+              <li class="list-group-item">Cámaras: {product.cameras}</li>
+              <li class="list-group-item">Dimensiones: {product.dimensions}</li>
+              <li class="list-group-item">Peso: {product.weight}</li>
+            </ul>
+            <div class="card-actions">
+              <a href="#" class="card-link">
+                Action
+              </a>
+              <a href="#" class="card-link">
+                Action
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </ItemDescriptionStyles>
   );
 };
