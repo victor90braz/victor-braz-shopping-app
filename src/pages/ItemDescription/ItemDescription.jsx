12f6916d2eb7a32dce12dfff158155d3d066c792
victor90braz <victor.90braz@gmail.com>
2023-08-25 09:13:10 +0200

    Set layout to mobile

--- src/pages/ItemDescription/ItemDescription.jsx
+++ src/pages/ItemDescription/ItemDescription.jsx
@@ -27,62 +27,77 @@ const ItemDescription = () => {
           <FontAwesomeIcon icon={faSpinner} spin />
         </div>
       ) : product && Object.keys(product).length > 0 ? (
-        <div className="card" style="width: 52rem">
-          <img
-            src={product.imgUrl}
-            alt={product.name}
-            className="full-width-image"
-          />
-          <div className="card-body">
-            <h5 className="card-title">{product.brand}</h5>
-            <ul className="list-group list-group-flush">
-              {product.brand && (
-                <li className="list-group-item">Marca: {product.brand}</li>
-              )}
-              {product.model && (
-                <li className="list-group-item">Modelo: {product.model}</li>
-              )}
-              {product.price && (
-                <li className="list-group-item">Precio: {product.price}€</li>
-              )}
-              {product.cpu && (
-                <li className="list-group-item">CPU: {product.cpu}</li>
-              )}
-              {product.ram && (
-                <li className="list-group-item">RAM: {product.ram}</li>
-              )}
-              {product.os && (
-                <li className="list-group-item">
-                  Sistema Operativo: {product.os}
-                </li>
-              )}
-              {product.screenResolution && (
-                <li className="list-group-item">
-                  Resolución de pantalla: {product.screenResolution}
-                </li>
-              )}
-              {product.battery && (
-                <li className="list-group-item">Batería: {product.battery}</li>
-              )}
-              {product.cameras && (
-                <li className="list-group-item">Cámaras: {product.cameras}</li>
-              )}
-              {product.dimensions && (
-                <li className="list-group-item">
-                  Dimensiones: {product.dimensions}
-                </li>
-              )}
-              {product.weight && (
-                <li className="list-group-item">Peso: {product.weight}</li>
-              )}
-            </ul>
+        <div className="card">
+          <div className="container-image">
+            <img
+              src={product.imgUrl}
+              alt={product.name}
+              className="full-width-image"
+            />
+          </div>
+
+          <div className="container-body">
+            <div className="card-body">
+              <ul className="list-group list-group-flush">
+                {product.brand && (
+                  <li className="list-group-item">Marca: {product.brand}</li>
+                )}
+                {product.model && (
+                  <li className="list-group-item">Modelo: {product.model}</li>
+                )}
+                {product.price && (
+                  <li className="list-group-item">Precio: {product.price}€</li>
+                )}
+                {product.cpu && (
+                  <li className="list-group-item">CPU: {product.cpu}</li>
+                )}
+                {product.ram && (
+                  <li className="list-group-item">RAM: {product.ram}</li>
+                )}
+                {product.os && (
+                  <li className="list-group-item">
+                    Sistema Operativo: {product.os}
+                  </li>
+                )}
+                {product.screenResolution && (
+                  <li className="list-group-item">
+                    Resolución de pantalla: {product.screenResolution}
+                  </li>
+                )}
+                {product.battery && (
+                  <li className="list-group-item">
+                    Batería: {product.battery}
+                  </li>
+                )}
+                {product.cameras && (
+                  <li className="list-group-item">
+                    Cámaras: {product.cameras}
+                  </li>
+                )}
+                {product.dimensions && (
+                  <li className="list-group-item">
+                    Dimensiones: {product.dimensions}
+                  </li>
+                )}
+                {product.weight && (
+                  <li className="list-group-item">Peso: {product.weight}</li>
+                )}
+              </ul>
+            </div>
+
             <div className="card-actions">
-              <a href="#" className="card-link">
-                Action
-              </a>
-              <a href="#" className="card-link">
-                Action
-              </a>
+              <div className="container-actions">
+                <button>Delete</button>
+                <button>Delete</button>
+                <button>Delete</button>
+              </div>
+              <div className="container-actions">
+                <button>Delete</button>
+                <button>Delete</button>
+              </div>
+              <div className="container-actions">
+                <button>Delete</button>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/ItemDescription/ItemDescriptionStyles.jsx
+++ src/pages/ItemDescription/ItemDescriptionStyles.jsx
@@ -4,28 +4,44 @@ export const ItemDescriptionStyles = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 90vh;
+  height: 80vh;
+
+  @media (max-width: 715px) {
+    height: 100vh;
+    margin-top: -60px;
+  }
 
   .card {
+    width: 52rem;
+    padding: 15px;
     display: flex;
     margin: 10px;
     margin-right: 0.5rem !important;
     flex-direction: row;
-    align-items: center;
+    border: 1px solid gray;
 
-    @media (max-width: 520px) {
+    @media (max-width: 715px) {
       flex-direction: column;
     }
   }
 
-  .card-image {
-    height: 100%;
-    width: 100%;
+  .container-image {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    max-height: 300px;
+
+    @media (max-width: 715px) {
+      justify-content: center;
+    }
   }
 
-  .full-width-image {
-    flex: 0.5;
-    padding: 20px;
+  .container-body {
+    flex: 1.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
 
   .card-body {
@@ -40,5 +56,27 @@ export const ItemDescriptionStyles = styled.div`
 
   .list-group-item {
     padding: 0;
+  }
+
+  .card-actions,
+  .card-body {
+    min-width: 424px;
+    padding: 20px;
+  }
+
+  .card-actions {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container-actions {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: center;
+  }
+
+  .container-actions button {
+    margin: 10px;
   }
 `;
