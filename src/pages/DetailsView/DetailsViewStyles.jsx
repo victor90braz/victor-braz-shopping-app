40e45ba7382912ba14ac1288fd06b14fe2d377a3
victor90braz <victor.90braz@gmail.com>
2023-08-25 10:03:22 +0200

    Clean up

--- src/pages/DetailsView/DetailsViewStyles.jsx
+++ src/pages/DetailsView/DetailsViewStyles.jsx
@@ -6,7 +6,6 @@ export const DetailsViewStyles = styled.section`
   align-items: center;
   margin: 10px;
   border-radius: 10px;
-
   background-color: #10020212;
   flex-direction: column;
   height: 80vh;
