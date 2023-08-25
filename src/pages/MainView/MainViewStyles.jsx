2e64dac63de8c1224cddb62b45aec0b590e47463
victor90braz <victor.90braz@gmail.com>
2023-08-25 10:49:18 +0200

    Decrease padding of MainViewStyles

--- src/pages/MainView/MainViewStyles.jsx
+++ src/pages/MainView/MainViewStyles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const MainViewStyles = styled.section`
-  padding: 30px;
+  padding: 9px;
   background-color: #10020212;
   margin: 10px;
   border-radius: 10px;
