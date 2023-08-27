29e00e455c6ea03fb50e307b9cacbf43d98e6b05
victor90braz <victor.90braz@gmail.com>
2023-08-27 15:21:17 +0200

    Create middleware for Redux. Middleware in Redux is a way to intercept dispatched actions and add extra behavior to them before they reach the reducers

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -1,10 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunkMiddleware from "redux-thunk";
 import productsReducer from "../features/sliceProducts";
+
+const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+  next(action);
+  localStorage.setItem("__redux__state", JSON.stringify(store.getState()));
+};
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
   },
+  middleware: [thunkMiddleware, persistanceLocalStorageMiddleware],
 });
 
 export default store;
