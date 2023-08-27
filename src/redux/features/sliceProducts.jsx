a4502b65cd6a3e0661d9816d991f2fb51d498149
victor90braz <victor.90braz@gmail.com>
2023-08-27 15:27:09 +0200

    Assign to initial state parse the products property from the persisted state; otherwise, it will default to an empty array

--- src/redux/features/sliceProducts.jsx
+++ src/redux/features/sliceProducts.jsx
@@ -1,8 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = (() => {
+  const persistedState = localStorage.getItem("__redux_state__");
+
+  return persistedState ? JSON.parse(persistedState).products : [];
+})();
+
 const sliceProducts = createSlice({
   name: "products",
-  initialState: [],
+  initialState,
   reducers: {
     loadProducts: (state, action) => action.payload,
     loadSingleProduct: (currentData, action) => {
