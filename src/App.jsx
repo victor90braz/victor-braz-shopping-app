70260f97299ac202509218c727656a5b4e39b5a3
victor90braz <victor.90braz@gmail.com>
2023-08-28 14:03:45 +0200

    Back to previous dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,8 @@ import { NotFound } from "./pages/_404.jsx";
 import MainView from "./pages/MainView/MainView";
 import ActionsCartPage from "./pages/ActionsCartPage/ActionsCartPage.jsx";
 import DetailsView from "./pages/DetailsView/DetailsView.jsx";
-import { thunkLoadProducts } from "./redux/thunks/thunksProducts.jsx";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 export const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(thunkLoadProducts());
-  }, [dispatch]);
-
   return (
     <LocationProvider>
       <Header />
